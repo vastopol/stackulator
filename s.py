459e7782b6d9d20e0a3da498168183a4f1126535
vastopol <classicunix@gmail.com>
2018-10-19 19:53:26 -0700

    basic rpn expressions working

--- s.py
+++ s.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+# RPN calculator
+
 # Modules
 import sys
 
@@ -11,14 +13,39 @@ dstk = []
 def main(argv):
     global fstk, dstk
     while True:
+        print("")
         txt1 = input(">> ")
         txt2 = txt1.split()
         for t in txt2:
-            if t.isdigit():
-                dstk.append(t)
-            else:
+            try:
+                x = int(t)
+                dstk.append(x)
+            except:
+
                 fstk.append(t)
-        print(fstk,dstk)
+        show()
+        calc()
+        #show()
+
+def calc():
+    global fstk, dstk
+    while not len(fstk) == 0:
+        if len(dstk) >= 2:
+            a = dstk.pop(0)
+            b = dstk.pop(0)
+            f = fstk.pop(0)
+            r = eval(str(a) + str(f) + str(b))
+            dstk.insert(0,r)
+            show()
+        else:
+            return
+
+# stack print
+def show():
+    global fstk, dstk
+    print("")
+    print("func: ",fstk)
+    print("data: ",dstk)
 
 #----------------------------------------
 
